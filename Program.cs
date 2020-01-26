5666f3d4bd47df7bf6d219babd486773e3cb9e22
Shan Liang <51506048+shanliang1984@users.noreply.github.com>
2020-01-26 13:06:55 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FrameInfoCIGI;
+
+namespace TestCoreDll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IGControl myig = new IGControl();
+
+            myig.DatabaseNumber = 5;
+            myig.IGMode = 2;
+            myig.MinorVersion = 3;
+            myig.TimestampValid = 1;
+            myig.ByteSwapMagicNumber = 80;
+            myig.HostFrameNumber = 101;
+            myig.LastIGFrameNumber = 203;
+            myig.Timestamp = 99;
+
+            IGControl.Save(myig, "test.xml");
+
+            IGControl readig = IGControl.Load("test.xml");
+
+            Console.WriteLine(readig.ToString());
+        }
+    }
+}
